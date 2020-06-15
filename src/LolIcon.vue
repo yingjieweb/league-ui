419c8d96e85f83bcc14a9ee93817c23c11f8ff04
yingjieweb <1184061078@qq.com>
2020-06-15 15:25:01 +0800

    Encapsulate icon component

---
+++ src/LolIcon.vue
@@ -0,0 +1,22 @@
+<template>
+  <svg class="lol-icon">
+    <use :xlink:href="`#lol-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+  export default {
+    name: "LolIcon",
+    props: ['name']
+  }
+</script>
+
+<style lang="scss" scoped>
+  .lol-icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+</style>
