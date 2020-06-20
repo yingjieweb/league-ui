48b2efe98425120f583bf71dcf00816654fae291
yingjieweb <1184061078@qq.com>
2020-06-20 16:10:30 +0800

    npm main entry

---
+++ index.js
@@ -0,0 +1,5 @@
+import LolButton from "./src/components/Button/LolButton";
+import LolButtonGroup from "./src/components/Button/LolButtonGroup";
+import LolIcon from "./src/components/Icon/LolIcon";
+
+export {LolButton, LolButtonGroup, LolIcon}
