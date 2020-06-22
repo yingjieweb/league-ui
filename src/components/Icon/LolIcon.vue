dfb4c0e738c0fa7e561b454f91e9eb64d25da5de
yingjieweb <1184061078@qq.com>
2020-06-22 10:38:31 +0800

    optimize the icon component

--- src/components/Icon/LolIcon.vue
+++ src/components/Icon/LolIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg class="lol-icon">
+  <svg v-if="name" :class="classes" :style="styles" @click="$emit('click', $event)">
     <use :xlink:href="`#lol-${name}`"></use>
   </svg>
 </template>
@@ -9,16 +9,50 @@
 
   export default {
     name: "LolIcon",
-    props: ['name']
+    props: {
+      name: {
+        type: String,
+        default: ''
+      },
+      color: {
+        type: String,
+        default: '#C9D4E3'
+      },
+      isLoading: {
+        type: Boolean,
+        default: false
+      },
+      size: {
+        type: String,
+        default: '1em'
+      }
+    },
+    computed: {
+      styles() {
+        return {
+          fill: this.color,
+          width: this.size,
+          height: this.size
+        }
+      },
+      classes() {
+        return [
+          'lol-icon',
+          {'lol-icon_loading': this.isLoading}
+        ]
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   .lol-icon {
-    width: 1em;
-    height: 1em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+
+    &_loading {
+      animation: spin .5s infinite linear;
+    }
   }
 </style>
