d32da1b33ac2ed145715b62fd39d751cda7b9a0a
yingjieweb <1184061078@qq.com>
2020-06-25 11:06:27 +0800

    preliminary implement responsive layout

--- src/components/Layoyt/LolCol.vue
+++ src/components/Layoyt/LolCol.vue
@@ -1,30 +1,52 @@
 <template>
-  <div :class="classes" :style="styles">
+  <div class="lol-col" :class="classes" :style="styles">
     <slot></slot>
   </div>
 </template>
 
 <script>
+  let responsiveValidator = (layout) => {
+    let keys = Object.keys(layout)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
+      }
+    })
+    return valid
+  }
   export default {
     name: "LolCol",
     props: {
-      span: {
-        type: [Number, String]
-      },
-      offset: {
-        type: [Number, String]
-      }
+      span: { type: [Number, String] },
+      offset: { type: [Number, String] },
+      phone: { type: Object, validator: responsiveValidator },
+      iPad: { type: Object, validator: responsiveValidator },
+      narrowPc: { type: Object, validator: responsiveValidator },
+      pc: { type: Object, validator: responsiveValidator },
+      widePc: { type: Object, validator: responsiveValidator }
     },
     data() {
       return { gutter: 0 }
     },
     computed: {
       classes() {
-        return [
-          'lol-col',
-          this.span && `lol-col-${this.span}`,
-          this.offset && `offset-${this.offset}`
+        const devices = ['phone', 'iPad', 'narrowPC', 'pc', 'widePc']
+        const classes = [
+          this.span && `lol-col-default-span-${this.span}`,
+          this.offset && `lol-col-default-offset-${this.offset}`
         ]
+        devices.forEach((device) => {
+          if (this[device]) {
+            if (typeof this[device].span === 'number' && this[device].span !== 0) {
+              classes.push(`lol-col-${device}-span-${this[device].span}`)
+            }
+            if (typeof this[device].offset === 'number' && this[device].offset !== 0) {
+              classes.push(`lol-col-${device}-offset-${this[device].offset}`)
+            }
+          }
+        })
+        return classes
       },
       styles() {
         return {
@@ -38,17 +60,97 @@
 
 <style lang="scss" scoped>
   .lol-col {
-    $span-class-prefix: lol-col-;
+    $default-span-prefix: lol-col-default-span-;
     @for $n from 1 through 24 {
-      &.#{$span-class-prefix}#{$n} {
+      &.#{$default-span-prefix}#{$n} {
         width: ($n/24)*100%;
       }
     }
 
-    $offset-class-prefix: offset-;
+    $default-offset-prefix: lol-col-default-offset-;
     @for $n from 1 through 24 {
-      &.#{$offset-class-prefix}#{$n} {
+      &.#{$default-offset-prefix}#{$n} {
         margin-left: ($n/24)*100%;
+      }
+    }
+
+    @media (max-width: 576px) { /*phone*/
+      $phone-span-prefix: lol-col-phone-span-;
+      @for $n from 1 through 24 {
+        &.#{$phone-span-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+
+      $phone-offset-prefix: lol-col-phone-offset-;
+      @for $n from 1 through 24 {
+        &.#{$phone-offset-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+
+    @media (min-width: 577px) and (max-width: 768px) {  /*iPad*/
+      $iPad-span-prefix: lol-col-iPad-span-;
+      @for $n from 1 through 24 {
+        &.#{$iPad-span-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+
+      $iPad-offset-prefix: lol-col-iPad-offset-;
+      @for $n from 1 through 24 {
+        &.#{$iPad-offset-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+
+    @media (min-width: 769px) and (max-width: 992px) {  /*narrowPc*/
+      $narrowPc-span-prefix: lol-col-narrowPc-span-;
+      @for $n from 1 through 24 {
+        &.#{$narrowPc-span-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+
+      $narrowPc-offset-prefix: lol-col-narrowPc-offset-;
+      @for $n from 1 through 24 {
+        &.#{$narrowPc-offset-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+
+    @media (min-width: 993px) and (max-width: 1200px) {  /*pc*/
+      $pc-span-prefix: lol-col-pc-span-;
+      @for $n from 1 through 24 {
+        &.#{$pc-span-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+
+      $pc-offset-prefix: lol-col-pc-offset-;
+      @for $n from 1 through 24 {
+        &.#{$pc-offset-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
+      }
+    }
+
+    @media (min-width: 1200px) {  /*widePc*/
+      $widePc-span-prefix: lol-col-widePc-span-;
+      @for $n from 1 through 24 {
+        &.#{$widePc-span-prefix}#{$n} {
+          width: ($n/24)*100%;
+        }
+      }
+
+      $widePc-offset-prefix: lol-col-widePc-offset-;
+      @for $n from 1 through 24 {
+        &.#{$widePc-offset-prefix}#{$n} {
+          margin-left: ($n/24)*100%;
+        }
       }
     }
   }
--- src/components/Layoyt/LolRow.vue
+++ src/components/Layoyt/LolRow.vue
@@ -44,7 +44,7 @@
 <style lang="scss" scoped>
   .lol-row {
     display: flex;
-    flex-wrap: nowrap;
+    flex-wrap: wrap;
 
     &-align-left {
       justify-content: flex-start;
