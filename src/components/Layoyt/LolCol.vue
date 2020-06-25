17ee577e3ae9683e5ffd444b13a8a2ac059f879e
yingjieweb <1184061078@qq.com>
2020-06-25 11:49:21 +0800

    mobile terminal priority and support span = 0

--- src/components/Layoyt/LolCol.vue
+++ src/components/Layoyt/LolCol.vue
@@ -20,7 +20,6 @@
     props: {
       span: { type: [Number, String] },
       offset: { type: [Number, String] },
-      phone: { type: Object, validator: responsiveValidator },
       iPad: { type: Object, validator: responsiveValidator },
       narrowPc: { type: Object, validator: responsiveValidator },
       pc: { type: Object, validator: responsiveValidator },
@@ -31,17 +30,17 @@
     },
     computed: {
       classes() {
-        const devices = ['phone', 'iPad', 'narrowPC', 'pc', 'widePc']
+        const devices = ['iPad', 'narrowPC', 'pc', 'widePc']
         const classes = [
-          this.span && `lol-col-default-span-${this.span}`,
-          this.offset && `lol-col-default-offset-${this.offset}`
+          this.span && `lol-col-phone-span-${this.span}`,
+          this.offset && `lol-col-phone-offset-${this.offset}`
         ]
         devices.forEach((device) => {
           if (this[device]) {
-            if (typeof this[device].span === 'number' && this[device].span !== 0) {
+            if (typeof this[device].span === 'number') {
               classes.push(`lol-col-${device}-span-${this[device].span}`)
             }
-            if (typeof this[device].offset === 'number' && this[device].offset !== 0) {
+            if (typeof this[device].offset === 'number') {
               classes.push(`lol-col-${device}-offset-${this[device].offset}`)
             }
           }
@@ -60,78 +59,62 @@
 
 <style lang="scss" scoped>
   .lol-col {
-    $default-span-prefix: lol-col-default-span-;
-    @for $n from 1 through 24 {
-      &.#{$default-span-prefix}#{$n} {
+    $phone-span-prefix: lol-col-phone-span-;
+    @for $n from 0 through 24 {
+      &.#{$phone-span-prefix}#{$n} {
         width: ($n/24)*100%;
       }
     }
 
-    $default-offset-prefix: lol-col-default-offset-;
-    @for $n from 1 through 24 {
-      &.#{$default-offset-prefix}#{$n} {
+    $phone-offset-prefix: lol-col-phone-offset-;
+    @for $n from 0 through 24 {
+      &.#{$phone-offset-prefix}#{$n} {
         margin-left: ($n/24)*100%;
       }
     }
 
-    @media (max-width: 576px) { /*phone*/
-      $phone-span-prefix: lol-col-phone-span-;
-      @for $n from 1 through 24 {
-        &.#{$phone-span-prefix}#{$n} {
-          width: ($n/24)*100%;
-        }
-      }
-
-      $phone-offset-prefix: lol-col-phone-offset-;
-      @for $n from 1 through 24 {
-        &.#{$phone-offset-prefix}#{$n} {
-          margin-left: ($n/24)*100%;
-        }
-      }
-    }
-
-    @media (min-width: 577px) and (max-width: 768px) {  /*iPad*/
+    @media (min-width: 577px) {  /*iPad*/
       $iPad-span-prefix: lol-col-iPad-span-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$iPad-span-prefix}#{$n} {
           width: ($n/24)*100%;
         }
       }
 
       $iPad-offset-prefix: lol-col-iPad-offset-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$iPad-offset-prefix}#{$n} {
           margin-left: ($n/24)*100%;
         }
       }
     }
 
-    @media (min-width: 769px) and (max-width: 992px) {  /*narrowPc*/
+    @media (min-width: 769px) {  /*narrowPc*/
       $narrowPc-span-prefix: lol-col-narrowPc-span-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$narrowPc-span-prefix}#{$n} {
           width: ($n/24)*100%;
         }
       }
 
       $narrowPc-offset-prefix: lol-col-narrowPc-offset-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$narrowPc-offset-prefix}#{$n} {
           margin-left: ($n/24)*100%;
         }
       }
     }
 
-    @media (min-width: 993px) and (max-width: 1200px) {  /*pc*/
+    @media (min-width: 993px) {  /*pc*/
       $pc-span-prefix: lol-col-pc-span-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$pc-span-prefix}#{$n} {
           width: ($n/24)*100%;
         }
       }
 
       $pc-offset-prefix: lol-col-pc-offset-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$pc-offset-prefix}#{$n} {
           margin-left: ($n/24)*100%;
         }
@@ -140,14 +123,14 @@
 
     @media (min-width: 1200px) {  /*widePc*/
       $widePc-span-prefix: lol-col-widePc-span-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$widePc-span-prefix}#{$n} {
           width: ($n/24)*100%;
         }
       }
 
       $widePc-offset-prefix: lol-col-widePc-offset-;
-      @for $n from 1 through 24 {
+      @for $n from 0 through 24 {
         &.#{$widePc-offset-prefix}#{$n} {
           margin-left: ($n/24)*100%;
         }
