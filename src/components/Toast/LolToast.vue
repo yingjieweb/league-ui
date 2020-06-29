574bb2d7ce8b8473e5d182272b34bbb256c65cb5
yingjieweb <1184061078@qq.com>
2020-06-29 10:53:35 +0800

    solve toast double closing bug

--- src/components/Toast/LolToast.vue
+++ src/components/Toast/LolToast.vue
@@ -61,6 +61,7 @@
       },
       close() {
         this.$el.remove()
+        this.$emit('close')
         this.$destroy()
       },
       executeAutoClose() {
--- src/plugins/toast.js
+++ src/plugins/toast.js
@@ -8,19 +8,27 @@ export default {
       if (currentToast) {
         currentToast.close()
       }
-      currentToast = createToast({Vue, message, propsData: toastOptions})
+      currentToast = createToast({
+        Vue,
+        message,
+        propsData: toastOptions,
+        onClose: () => {
+          currentToast = null
+        }
+      })
     }
   }
 }
 
 /*helper*/
-function createToast({Vue, message, propsData}) {
+function createToast({Vue, message, propsData, onClose}) {
   let Constructor = Vue.extend(LolToast)
   let toast = new Constructor({
     propsData: propsData
   })
   toast.$slots.default = message
   toast.$mount()
+  toast.$on('close', onClose)
   document.body.appendChild(toast.$el)
   return toast
 }
