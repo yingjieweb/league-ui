aef0578c26ae76aa3b3b824e1c1f4d04740c3be8
yingjieweb <1184061078@qq.com>
2020-06-29 16:29:05 +0800

    add toast component test case

---
+++ test/unit/Toast/LolToast.test.js
@@ -0,0 +1,68 @@
+import Vue from 'vue'
+
+const expect = chai.expect
+import LolToast from "../../../src/components/Toast/LolToast"
+
+Vue.config.productionTip = false
+Vue.config.devtools = false
+
+describe('LolToast.vue', () => {
+
+  it('exist.', () => {
+    expect(LolToast).to.exist
+  })
+
+  describe('props', function () {
+    it('renders props.autoClose when passed', (done) => {
+      let div = document.createElement('div')
+      document.body.appendChild(div)
+      const Constructor = Vue.extend(LolToast)
+      const vm = new Constructor({
+        propsData: {
+          autoClose: 1
+        }
+      }).$mount(div)
+      vm.$on('close', () => {
+        expect(document.body.contains(vm.$el)).to.eq(false)
+        done()
+      })
+    })
+    it('renders props.closeButton when passed', () => {
+      const callback = sinon.fake();
+      const Constructor = Vue.extend(LolToast)
+      const vm = new Constructor({
+        propsData: {
+          closeButton: {
+            text: '关闭toast',
+            callback,
+          },
+        }
+      }).$mount()
+      let closeButton = vm.$el.querySelector('.closeButton')
+      expect(closeButton.textContent.trim()).to.eq('关闭toast')
+      closeButton.click()
+      expect(callback).to.have.been.called
+    })
+    it('renders props.enableHtml when passed', () => {
+      const Constructor = Vue.extend(LolToast)
+      const vm = new Constructor({
+        propsData: {
+          enableHtml: true
+        }
+      })
+      vm.$slots.default = `<strong id="test">hi</strong>`
+      vm.$mount()
+      let strong = vm.$el.querySelector('#test')
+      expect(strong).to.exist
+    })
+    it('接受 position', () => {
+      const Constructor = Vue.extend(LolToast)
+      const vm = new Constructor({
+        propsData: {
+          position: 'bottom'
+        }
+      }).$mount()
+      expect(vm.$el.classList.contains('lol-toast-position-bottom')).to.eq(true)
+    })
+  })
+})
