a8670d1a6473815324665057d363c454c5abd54f
yingjieweb <yingjieweb@qq.com>
2024-07-14 22:24:31 +0800

    style: 💄 Use calc instead of / to implement style compute

--- src/components/Layoyt/LolCol.vue
+++ src/components/Layoyt/LolCol.vue
@@ -62,14 +62,14 @@
     $phone-span-prefix: lol-col-span-;
     @for $n from 0 through 24 {
       &.#{$phone-span-prefix}#{$n} {
-        width: ($n/24)*100%;
+        width: calc($n / 24)*100%;
       }
     }
 
     $phone-offset-prefix: lol-col-offset-;
     @for $n from 0 through 24 {
       &.#{$phone-offset-prefix}#{$n} {
-        margin-left: ($n/24)*100%;
+        margin-left: calc($n / 24)*100%;
       }
     }
 
@@ -77,14 +77,14 @@
       $iPad-span-prefix: lol-col-iPad-span-;
       @for $n from 0 through 24 {
         &.#{$iPad-span-prefix}#{$n} {
-          width: ($n/24)*100%;
+          width: calc($n / 24)*100%;
         }
       }
 
       $iPad-offset-prefix: lol-col-iPad-offset-;
       @for $n from 0 through 24 {
         &.#{$iPad-offset-prefix}#{$n} {
-          margin-left: ($n/24)*100%;
+          margin-left: calc($n / 24)*100%;
         }
       }
     }
@@ -93,14 +93,14 @@
       $narrowPc-span-prefix: lol-col-narrowPc-span-;
       @for $n from 0 through 24 {
         &.#{$narrowPc-span-prefix}#{$n} {
-          width: ($n/24)*100%;
+          width: calc($n / 24)*100%;
         }
       }
 
       $narrowPc-offset-prefix: lol-col-narrowPc-offset-;
       @for $n from 0 through 24 {
         &.#{$narrowPc-offset-prefix}#{$n} {
-          margin-left: ($n/24)*100%;
+          margin-left: calc($n / 24)*100%;
         }
       }
     }
@@ -109,14 +109,14 @@
       $pc-span-prefix: lol-col-pc-span-;
       @for $n from 0 through 24 {
         &.#{$pc-span-prefix}#{$n} {
-          width: ($n/24)*100%;
+          width: calc($n / 24)*100%;
         }
       }
 
       $pc-offset-prefix: lol-col-pc-offset-;
       @for $n from 0 through 24 {
         &.#{$pc-offset-prefix}#{$n} {
-          margin-left: ($n/24)*100%;
+          margin-left: calc($n / 24)*100%;
         }
       }
     }
@@ -125,14 +125,14 @@
       $widePc-span-prefix: lol-col-widePc-span-;
       @for $n from 0 through 24 {
         &.#{$widePc-span-prefix}#{$n} {
-          width: ($n/24)*100%;
+          width: calc($n / 24)*100%;
         }
       }
 
       $widePc-offset-prefix: lol-col-widePc-offset-;
       @for $n from 0 through 24 {
         &.#{$widePc-offset-prefix}#{$n} {
-          margin-left: ($n/24)*100%;
+          margin-left: calc($n / 24)*100%;
         }
       }
     }
